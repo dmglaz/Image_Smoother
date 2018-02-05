210e173fc83e0df7c8b77eb9bd9675c445f34c1a
DmGlaz <dimag887@gmail.com>
2018-02-05 19:53:21 +0200

    averaging filter test
    Signed-off-by: DmGlaz <dimag887@gmail.com>

--- Main.py
+++ Main.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import cv2 as cv
+import cv2
 
+dirty_pics = {1: "1.png",2: "2.jpg"}
 
-print cv.__version__
-print np.array([1,2,2,3])
+img = cv2.imread('Dirty_Images\\'+dirty_pics[2])
 
-print "hello World1"
+kernel = np.ones((5,5), np.float32)/25
+dst = cv2.filter2D(img,-1,kernel)
 
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.show()
