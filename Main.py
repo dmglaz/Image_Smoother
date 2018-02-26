1ac198aab949943fbc916f54a4bc64d8e63878d7
DmGlaz <dimag887@gmail.com>
2018-02-26 19:12:00 +0200

    kernel(filter mask added) added
    Signed-off-by: DmGlaz <dimag887@gmail.com>

--- Main.py
+++ Main.py
@@ -8,7 +8,24 @@ from functions import *
 folder_path = "Dirty_Images\\"
 dirty_pics = {1: "1.png",2: "2.jpg"}
 
-convolution_2D_filter(folder_path+dirty_pics[2], 3)
+kernel = {
+    "sharpen": np.array([[0,-1,0],[-1,5,-1],[0,-1,0]]),
+    "edge_dtct1": np.array([[1,0,-1],[0,0,0],[-1,0,1]]),
+    "edge_dtct2": np.array([[1, 0, -1], [0, 0, 0], [-1, 0, 1]]),
+    "edge_dtct1": np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]]),
+    "edge_enhance": np.array([[0, 0, 0], [-1, 1, 0], [0, 0, 0]]),
+    "average": np.ones((3, 3), np.float32) / pow(3,2),
+    "gauss_blur33": np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])/16,
+    "gauss_blur55": np.array([[1, 4,6,4, 1], [ 4, 16, 24, 16,  4], [ 6, 24, 36, 24,  6], [ 4, 16, 24, 16,  4],[1, 4,6,4, 1] ]) / 256,
+    "unsharp_masking55": np.array([[1, 4,6,4, 1], [ 4, 16, 24, 16,  4], [ 6, 24, -476, 24,  6], [ 4, 16, 24, 16,  4],[1, 4,6,4, 1]])* (-1/256)
+}
+img = cv2.imread(folder_path+dirty_pics[2])
+
+new_img = convolution_2D_filter(img, kernel["gauss_blur33"])
+img2 = convolution_2D_filter(new_img, kernel["sharpen"])
+
+show_2_images(img,img2)
+
 averaging_smoother(folder_path+dirty_pics[2])
-gaussian_smoother(folder_path+dirty_pics[2])
-median_smoother(folder_path+dirty_pics[2])
+# gaussian_smoother(folder_path+dirty_pics[2])
+# median_smoother(folder_path+dirty_pics[2])
